d44854629559c8ff53b240fcc5fb1938813b66ee
GaoNeng-wWw <gaonenggg@outlook.com>
2024-11-18 21:17:59 +0800

    refactor: extract api,state,event init to index.ts

--- packages/renderless/src/color-select-panel/hue-select/vue.ts
+++ packages/renderless/src/color-select-panel/hue-select/vue.ts
@@ -1,76 +1,26 @@
 import type { IColorSelectPanelHueProps, ISharedRenderlessParamHooks, ISharedRenderlessParamUtils } from '@/types'
 import type { Color } from '../utils/color'
-import { getClientXY } from '../utils/getClientXY'
 import { draggable } from '../utils/use-drag'
+import { initDom, initState, useEvent } from '.'
 
 export const api = ['state', 'onSvReady', 'bar', 'thumb', 'wrapper']
 
 export const renderless = (
   props: IColorSelectPanelHueProps<Color>,
-  { onMounted, reactive, ref, computed }: ISharedRenderlessParamHooks,
-  { emit }: ISharedRenderlessParamUtils
+  hooks: ISharedRenderlessParamHooks,
+  utils: ISharedRenderlessParamUtils
 ) => {
-  const thumb = ref<HTMLElement>()
-  const bar = ref<HTMLElement>()
-  const wrapper = ref<HTMLElement>()
-  const cursor = ref<HTMLElement>()
-
-  const thumbLeft = ref(0)
-  const thumbTop = ref(0)
-  const hueValue = computed(() => props.color.get('hue'))
-  const state = reactive({ hueValue, thumb, bar, wrapper, thumbTop })
-  const onSvReady = (update) => {
-    emit('svReady', update)
-  }
+  const { onMounted } = hooks
+  const { emit } = utils
+  const { thumb, bar, wrapper } = initDom(hooks)
+  const state = initState(props, hooks)
+  const { onSvReady, onDrag, update } = useEvent({ thumb, bar, wrapper }, state, props, utils)
   const api = {
     state,
     onSvReady,
     bar,
     thumb,
     wrapper
-  }
-  const getThumbLeft = () => {
-    if (!thumb.value) {
-      return 0
-    }
-    const hue = props.color.get('hue')
-    if (!wrapper.value) {
-      return 0
-    }
-    return Math.round((hue * (wrapper.value.offsetWidth - thumb.value.offsetWidth / 2)) / 360)
-  }
-  const getThumbTop = () => {
-    if (!thumb.value) {
-      return 0
-    }
-    const hue = props.color.get('hue')
-    if (!bar.value) {
-      return 0
-    }
-    return (hue * (bar.value.offsetHeight - thumb.value.offsetHeight / 2)) / 360
-  }
-  const update = () => {
-    thumbLeft.value = getThumbLeft()
-    thumbTop.value = getThumbTop()
-  }
-  const onDrag = (event: MouseEvent | TouchEvent) => {
-    if (!bar.value || !thumb.value) {
-      return
-    }
-    const el = bar.value
-    if (!el) {
-      return
-    }
-    const rect = el?.getBoundingClientRect()
-    const { clientY } = getClientXY(event)
-    let top = clientY - rect.top
-
-    top = Math.min(top, rect.height - thumb.value.offsetHeight / 2)
-    top = Math.max(thumb.value.offsetHeight / 2, top)
-    const hue = Math.round(((top - thumb.value.offsetHeight / 2) / (rect.height - thumb.value.offsetHeight)) * 360)
-    thumbTop.value = top
-    emit('hueUpdate', hue)
-    props.color.set('hue', hue)
   }
   onMounted(() => {
     if (!bar.value || !thumb.value) {
